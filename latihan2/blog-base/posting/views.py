1cd803bae4a89ec4ad4ca0e49540d957cb2b64a5
riswan29 <wandra290401@gmail.com>
2022-11-19 13:19:31 +0700

    menambhakan class untuk signup

--- latihan2/blog-base/posting/views.py
+++ latihan2/blog-base/posting/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from posting.models import Posting
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def index(request):
     postings = Posting.objects.all()
 
     return render(request, 'posting/index.html', {'postings': postings})
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
